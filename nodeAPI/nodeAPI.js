1f8ed49decb81fef15d08c95bcbc5ad583c164da
LucasPARISOT <lucas.parisot.pro@gmail.com>
2021-09-05 20:54:08 +0200

    url filtering nodeAPI.js

--- nodeAPI/nodeAPI.js
+++ nodeAPI/nodeAPI.js
@@ -3,44 +3,45 @@ const mongoClient = require('mongodb').MongoClient;
 
 const port = 3000
 const host = '127.0.0.1'
-const serverUrl = '/test'
 
 const mongoUrl = 'mongodb://localhost:27017/';
 const mongoDb = 'test';
 const mongoCollection = 'test';
 
-
 const server = http.createServer((request, response) => {
-    let data = []
 
-    request
-        .on('data', d => {
-            data.push(d)
-        })
-        .on('end', () => {
-            data = Buffer.concat(data).toString()
-            const json = JSON.parse(data);
+    if(request.url === '/test') {
+        let data = []
 
-            mongoClient.connect(mongoUrl, function(err, client) {
+        request
+            .on('data', d => {
+                data.push(d)
+            })
+            .on('end', () => {
+                data = Buffer.concat(data).toString()
+                const json = JSON.parse(data);
 
-                // TODO: Remove db test and associated collections
-                const db = client.db(mongoDb);
-                const collection = db.collection(mongoCollection);
+                mongoClient.connect(mongoUrl, function(err, client) {
 
-                const result = collection.insertOne(json);
+                    // TODO: Remove db test and associated collections
+                    const db = client.db(mongoDb);
+                    const collection = db.collection(mongoCollection);
 
-                console.log(`le document a été ajouté à la collection '${mongoCollection}' de la base de données '${mongoDb}'`);
-            });
+                    const result = collection.insertOne(json);
 
-            console.log(json)
+                    console.log(`le document a été ajouté à la collection '${mongoCollection}' de la base de données '${mongoDb}'`);
+                });
 
-            response.statusCode = 201
-            response.setHeader('Access-Control-Allow-Origin','*');
-            response.setHeader('Access-Control-Allow-Methods','GET, POST');
-            response.setHeader('Access-Control-Allow-Headers','X-Requested-With, Access-Control-Allow-Origin, Accept');
-            response.end()
-        })
+                console.log(json)
+
+                response.statusCode = 201
+                response.setHeader('Access-Control-Allow-Origin','*');
+                response.setHeader('Access-Control-Allow-Methods','GET, POST');
+                response.setHeader('Access-Control-Allow-Headers','X-Requested-With, Access-Control-Allow-Origin, Accept');
+                response.end()
+            })
+    }
 })
 
 server.listen(port, host)
-console.log(`Listening at http://${host}:${port}${serverUrl}`)
+console.log(`Listening at http://${host}:${port}`)
